d3dfb13c2de601e5ce683fee72565ed156d7bb14
Knar <knarxr@protonmail.com>
2025-04-02 16:14:25 -0400

    Initial Commit - anonymized name and added .gitignore

---
+++ ClassLibrary1/DivisibleCounter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class DivisibleCounter
+    {
+        public static string Count(int upperBound)
+        {
+            string countResult = "";
+
+            for (int i = 1; i <= upperBound; i++)
+            {
+                var divisibleByThree = false;
+                var divisibleByFive = false;
+                if (i % 3 == 0)
+                {
+                    divisibleByThree = true;
+                }
+                if (i % 5 == 0)
+                {
+                    divisibleByFive = true;
+                }
+
+                if (!divisibleByThree && !divisibleByFive)
+                {
+                    countResult += $"{i} ";
+                }
+
+                if (divisibleByThree)
+                {
+                    countResult += "FirstName ";
+                }
+                if (divisibleByFive)
+                {
+                    countResult += "LastName";
+                }
+                countResult += "\n";
+            }
+            return countResult;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary1;
+
+
+//numers 1-100 printed to screen
+//divisible by 3 - oprint "andrew"
+//divisible by 5 - print "byerly"
+
+//use in other programs
+//create a dotnet library, get output back
+//pass in upper bound, name still hardcoded
+
+//implement as stream
+//add unit test project, add unit tests to prove that it works, add regression tests
+//enhancement: parameratize name, number and word pairs (arbitrary number of number/word pairs)
+//push to public git repo
+
+Console.WriteLine(DivisibleCounter.Count(Int32.MaxValue));
